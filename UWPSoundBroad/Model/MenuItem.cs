f195343a4d4bf6855d527539e1ed5d2e479b51fd
OliverMengich <oliverkemei2000@gmail.com>
2019-10-28 18:46:18 +0300

    UWP sounds interface

---
+++ UWPSoundBroad/Model/MenuItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static UWPSoundBroad.Model.Sound;
+
+namespace UWPSoundBroad.Model
+{
+    class MenuItem
+    {
+        public string IconFile { get;set;}
+        public SoundCategory Category { get;set;}
+    }
+}
---
+++ UWPSoundBroad/Model/Sound.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPSoundBroad.Model
+{
+    class Sound
+    {
+        public string Name { get; set; }
+        public SoundCategory Category { get; set; }
+        public string AudioFile { get; set; }
+        public string ImageFile { get; set; }
+        public enum SoundCategory
+        {
+            Animals,
+            Cartoons,
+            Taunts,
+            Warnings
+        }
+        public Sound(string name,SoundCategory category)
+        {
+            Name = name;
+            Category = category;
+            AudioFile = String.Format("/Assets/Audio/{0}/{1}.mp3",category,name);
+            ImageFile = String.Format("/Assets/Images/{0}/{1}.jpg", category,name);
+
+        }
+    }
+}
